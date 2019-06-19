e0339c56bb53c821baf22f8de923d8c9d286ce96
Asmir <asmir.ibrovic@gmail.com>
2019-06-20 00:24:22 +0200

    migrations, locationFk

---
+++ WeatherControlApp/Migrations/20190619220414_locationfk.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeatherControlApp.Migrations
+{
+    public partial class locationfk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LocationId",
+                table: "WeatherItems",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WeatherItems_LocationId",
+                table: "WeatherItems",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WeatherItems_Location_LocationId",
+                table: "WeatherItems",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WeatherItems_Location_LocationId",
+                table: "WeatherItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WeatherItems_LocationId",
+                table: "WeatherItems");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "WeatherItems");
+        }
+    }
+}
